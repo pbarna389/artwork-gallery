48c3b4e75c7c828346c1ceb79f7fed9084d36e36
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2023-03-27 21:10:22 +0200

    Some pre-styling on swiper-slides

--- src/pages/Artists.tsx
+++ src/pages/Artists.tsx
@@ -54,7 +54,7 @@ const Artists = () => {
                     coverflowEffect={{
                         rotate: 50,
                         stretch: 0,
-                        depth: 100,
+                        depth: 50,
                         modifier: 1,
                         slideShadows: true,
                     }}
