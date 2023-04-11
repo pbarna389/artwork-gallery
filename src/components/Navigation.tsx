e0f5bc84f1a3da8af13b02047e9d380fb93f72ae
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2023-04-11 17:23:18 +0200

    navigation updated

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,18 +3,45 @@ import { Link } from "react-router-dom";
 import { artworkContext } from '../context/ArtworkContext';
 import { IArtworkContext } from '../@types/artwork';
 
+import Goya from "../assets/goya.jpg";
+import Greco from "../assets/greco.jpg";
+import Michelangelo from "../assets/michelangelo.jpg";
+import Dali from "../assets/dali.jpg";
+
 const Navigation = () => {
     const { artistPagination, artworksPagination, setNavShown, navShown } = useContext(artworkContext) as IArtworkContext;
     return (
         <nav className={`${navShown ? "shown" : "not-shown"}`}>
             <div className={`link-wrapper`}>
-                <Link to="/" onClick={e => setNavShown(!navShown)}>Home</Link>
+                <Link to="/" onClick={e => setNavShown(!navShown)}>
+                    <p>Home</p>
+                    <div>
+                        <img src={Greco} alt=""></img>
+                    </div>
+                </Link>
                 <div className="helper"></div>
-                <Link to={`artists/${artistPagination}`} onClick={e => setNavShown(!navShown)}>Artists</Link>
+                <Link to={`artists/${artistPagination}`} onClick={e => setNavShown(!navShown)}>
+                    <p>Artists</p>
+                    <div>
+                        <img src={Michelangelo} alt=""></img>
+                    </div>
+                </Link>
                 <div className="helper"></div>
-                <Link to={`artworks/${artworksPagination}`} onClick={e => setNavShown(!navShown)}>Artworks</Link>
+                <Link to={`artworks/${artworksPagination}`} onClick={e => setNavShown(!navShown)}>
+                    <p>
+                        Artworks
+                    </p>
+                    <div>
+                        <img src={Goya} alt=""></img>
+                    </div>
+                </Link>
                 <div className="helper"></div>
-                <Link to={`/profile`} onClick={e => setNavShown(!navShown)}>Profile</Link>
+                <Link to={`/profile`} onClick={e => setNavShown(!navShown)}>
+                    <p>Profile</p>
+                    <div>
+                        <img src={Dali} alt=""></img>
+                    </div>
+                </Link>
                 <div className="helper"></div>
             </div>
         </nav>
