dbea5101243573ff7be01868acef16fc592e8eb7
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2023-04-11 11:08:06 +0200

    logout navigation added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,8 @@ import Logo from './Logo';
 import Searchbar from './Searchbar';
 import Navigation from './Navigation';
 
+import { useNavigate } from "react-router-dom";
+
 import "../styles/components/Navbar.css"
 import { auth } from '../config/firebase-config';
 import { signOut } from 'firebase/auth';
@@ -13,11 +15,13 @@ import { signOut } from 'firebase/auth';
 const Navbar = () => {
 
     const { navShown, userDispatch } = useContext(artworkContext) as IArtworkContext;
+    const navigate = useNavigate();
 
     const logout = async () => {
         try {
             await signOut(auth);
             userDispatch({ type: "setLogin", payload: false });
+            navigate("/");
         } catch (error) {
             console.log(error)
         }
